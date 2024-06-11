c0c5bff13c03f27d5848c9053408d239fb984679
claudiomecenas <claudio@penseon.com.br>
2024-06-11 14:31:04 -0300

    exemple 1 doc PHPUnit

---
+++ tests/GreeterTest.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__ . '/../src/Greeter.php';
+
+final class GreeterTest extends TestCase
+{
+    public function testGreetsWithName(): void
+    {
+        $greeter = new Greeter;
+
+        $greeting = $greeter->greet('Alice');
+
+        $this->assertSame('Hello, Alice!', $greeting);
+    }
+}
